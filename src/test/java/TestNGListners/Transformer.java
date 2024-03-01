c76bc60c3b9e1545eabb3ce19b916245589b1bde
Admin <admin@gmail.com>
2024-03-01 17:31:45 +0530

    Added RLL DemoWebShop Project

---
+++ src/test/java/TestNGListners/Transformer.java
@@ -0,0 +1,15 @@
+package TestNGListners;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+import org.testng.IAnnotationTransformer;
+import org.testng.annotations.ITestAnnotation;
+
+public class Transformer implements IAnnotationTransformer {
+
+	@SuppressWarnings("rawtypes")
+	public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+		annotation.setRetryAnalyzer(TestNGListners.RetryAnalyzer.class);
+	}
+}
